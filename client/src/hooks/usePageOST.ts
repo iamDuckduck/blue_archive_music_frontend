9e6854a28c2164b26cae39217ac7a2e4155a45ce
duck <simonchungcck42@gmail.com>
2025-07-26 21:08:00 +0800

    feat: create api client and usePage function

---
+++ client/src/hooks/usePageOST.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../service/api-client";
+import ms from "ms";
+import type OstPage from "../entities/OstPage";
+import type { GridSortItem } from "../pages/OstGrid";
+import type { GridPaginationModel } from "@mui/x-data-grid";
+
+const apiClient = new APIClient<OstPage>("/user/ost");
+
+const usePageOST = (
+  paginationModel: GridPaginationModel,
+  sortModel: GridSortItem | undefined
+) => {
+  return useQuery({
+    queryKey: ["pageOST", paginationModel, sortModel],
+    queryFn: () =>
+      apiClient.getPageOST({
+        params: {
+          page: paginationModel.page,
+          size: paginationModel.pageSize,
+          fleid: sortModel?.field,
+          sort: sortModel?.sort,
+        },
+      }),
+    staleTime: ms("24h"),
+  });
+};
+
+export default usePageOST;
---
+++ client/src/service/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
+export interface FetchPageResponse<T> {
+  content: T[];
+  totalPages: number;
+  totalElements: number;
+  size: number;
+}
+
+// TODO use env variable to config baseURL bewteen dev and production
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  // use arrow function so no reference problem from this.endpoint
+  getPageOST = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchPageResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  getAudioOst = (id: number) => {
+    return axiosInstance
+      .get(this.endpoint + "/audio/" + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
