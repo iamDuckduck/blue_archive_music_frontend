c02fc9f48818ac154627d2429abc3667ac428de5
duck <simonchungcck42@gmail.com>
2025-07-26 21:00:38 +0800

    feat: homepage

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import Box from "@mui/material/Box";
+import { Button, Fade, Stack, Typography } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const boxStyle = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  };
+
+  const navigate = useNavigate();
+  return (
+    <Box sx={boxStyle}>
+      <Fade in={true} timeout={3000}>
+        <Stack
+          sx={{
+            marginBottom: 10,
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h1">Blue Archive OST</Typography>
+
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              opacity: 0.9,
+              padding: 1.5,
+            }}
+            onClick={() => {
+              navigate("/OST");
+            }}
+          >
+            Click here to play
+          </Button>
+        </Stack>
+      </Fade>
+    </Box>
+  );
+};
+
+export default Home;
