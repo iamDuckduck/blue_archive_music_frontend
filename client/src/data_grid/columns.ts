0a5264c09429f52c7cabdd9f5b8be9f1801c40f9
duck <simonchungcck42@gmail.com>
2025-08-08 21:32:29 +0800

    feat: limit filter option - "contains" for String operation, "=" for numeric

--- client/src/data_grid/columns.ts
+++ client/src/data_grid/columns.ts
@@ -1,9 +1,44 @@
-import type { GridColDef } from "@mui/x-data-grid";
+import {
+  getGridNumericOperators,
+  getGridStringOperators,
+  type GridColDef,
+} from "@mui/x-data-grid";
+
+const getContainsOperator = () =>
+  getGridStringOperators().filter((operator) => operator.value === "contains");
+
+const getEqualOperator = () =>
+  getGridNumericOperators().filter((operator) => operator.value === "=");
 
 export const ostColumns: GridColDef[] = [
-  { field: "ostNumber", headerName: "No.", flex: 0.3 },
-  { field: "name", headerName: "Name", flex: 1 },
-  { field: "author", headerName: "Author", flex: 1 },
-  { field: "volume", headerName: "Volume", flex: 0.3 },
-  { field: "volumeName", headerName: "Volume_name", flex: 1 },
+  {
+    field: "ostNumber",
+    headerName: "No.",
+    flex: 0.3,
+    filterOperators: getEqualOperator(),
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    flex: 1,
+    filterOperators: getContainsOperator(),
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    flex: 1,
+    filterOperators: getContainsOperator(),
+  },
+  {
+    field: "volume",
+    headerName: "Volume",
+    flex: 0.3,
+    filterOperators: getEqualOperator(),
+  },
+  {
+    field: "volumeName",
+    headerName: "Volume_name",
+    flex: 1,
+    filterOperators: getContainsOperator(),
+  },
 ];
