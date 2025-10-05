2d2e2b9969e61411c651e352d02ecc4da34d7028
duck <simonchungcck42@gmail.com>
2025-10-05 16:40:41 +0800

    feat: apply currentTrack context into Controls.tsx

--- client/src/Component/Controls.tsx
+++ client/src/Component/Controls.tsx
@@ -1,5 +1,12 @@
+import { useAudioPlayerContext } from "../context/audio-player-context";
+
 const Controls = () => {
-  return <div>Controls</div>;
+  const { currentTrack } = useAudioPlayerContext();
+  return (
+    <div>
+      <audio src={currentTrack.src} controls />
+    </div>
+  );
 };
 
 export default Controls;
