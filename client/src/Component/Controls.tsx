8cec5f57d262d3d21b10c1489498d201bb2b4fdc
duck <simonchungcck42@gmail.com>
2025-10-06 18:55:45 +0800

    feat: draft for control component

--- client/src/Component/Controls.tsx
+++ client/src/Component/Controls.tsx
@@ -1,12 +1,60 @@
+import { IconButton, Stack } from "@mui/material";
 import { useAudioPlayerContext } from "../context/audio-player-context";
+import {
+  BsSkipStartFill,
+  BsFillRewindFill,
+  BsFillPauseFill,
+  BsFillPlayFill,
+  BsFillFastForwardFill,
+  BsSkipEndFill,
+  BsShuffle,
+  BsRepeat,
+} from "react-icons/bs";
+import { useState } from "react";
 
 const Controls = () => {
   const { currentTrack } = useAudioPlayerContext();
+  const [isShuffle, setIsShuffle] = useState<boolean>(false);
+  const [isRepeat, setIsRepeat] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
   return (
-    <div>
-      <audio src={currentTrack.src} controls />
-    </div>
+    <Stack direction="row" spacing={1} alignItems="center">
+      <audio src={currentTrack.src} />
+      <IconButton onClick={() => {}} size="small">
+        <BsSkipStartFill size={20} />
+      </IconButton>
+      <IconButton onClick={() => {}} size="small">
+        <BsFillRewindFill size={20} />
+      </IconButton>
+      <IconButton onClick={() => setIsPlaying((prev) => !prev)} size="medium">
+        {isPlaying ? (
+          <BsFillPauseFill size={30} />
+        ) : (
+          <BsFillPlayFill size={30} />
+        )}
+      </IconButton>
+      <IconButton onClick={() => {}} size="small">
+        <BsFillFastForwardFill size={20} />
+      </IconButton>
+      <IconButton onClick={() => {}} size="small">
+        <BsSkipEndFill size={20} />
+      </IconButton>
+      <IconButton
+        onClick={() => setIsShuffle((prev) => !prev)}
+        size="small"
+        sx={{ color: isShuffle ? "#f50" : "inherit" }} // text-[#f50] equivalent
+      >
+        <BsShuffle size={20} />
+      </IconButton>
+      <IconButton
+        onClick={() => setIsRepeat((prev) => !prev)}
+        size="small"
+        sx={{ color: isRepeat ? "#f50" : "inherit" }} // text-[#f50] equivalent
+      >
+        <BsRepeat size={20} />
+      </IconButton>
+    </Stack>
   );
 };
-
 export default Controls;
