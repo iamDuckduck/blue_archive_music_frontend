87f438d653d57fad17bcbf3a278716e4aebd9213
duck <simonchungcck42@gmail.com>
2025-10-06 20:20:25 +0800

    feat: create context to handle audioPlayer states

--- client/src/context/audio-player-context.tsx
+++ client/src/context/audio-player-context.tsx
@@ -5,11 +5,14 @@ import {
   type ReactNode,
   type Dispatch,
   type SetStateAction,
+  type RefObject,
+  useRef,
 } from "react";
 import type OstPage from "../entities/OstPage";
 
 export interface Track {
-  title: string;
+  id: number;
+  name: string;
   src: string;
   author: string;
   thumbnail?: string;
@@ -19,6 +22,9 @@ interface AudioPlayerContextType {
   setCurrentTrack: Dispatch<SetStateAction<Track>>;
   trackList: OstPage[];
   setTrackList: Dispatch<SetStateAction<OstPage[]>>;
+  audioRef: RefObject<HTMLAudioElement | null>;
+  isPlaying: boolean;
+  setIsPlaying: Dispatch<SetStateAction<boolean>>;
 }
 const AudioPlayerContext = createContext<AudioPlayerContextType | undefined>(
   undefined
@@ -26,12 +32,17 @@ const AudioPlayerContext = createContext<AudioPlayerContextType | undefined>(
 export const AudioPlayerProvider = ({ children }: { children: ReactNode }) => {
   const [currentTrack, setCurrentTrack] = useState<Track>({} as Track);
   const [trackList, setTrackList] = useState<OstPage[]>({} as OstPage[]);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const contextValue = {
     currentTrack,
     setCurrentTrack,
     trackList,
     setTrackList,
+    audioRef,
+    isPlaying,
+    setIsPlaying,
   };
   return (
     <AudioPlayerContext.Provider value={contextValue}>
