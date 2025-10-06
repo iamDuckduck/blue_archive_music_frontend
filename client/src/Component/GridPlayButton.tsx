a209e6bd0cb81a204e238f215b25d87d0d2050d9
duck <simonchungcck42@gmail.com>
2025-10-06 20:28:58 +0800

    feat: fetch images for currentTrack

--- client/src/Component/GridPlayButton.tsx
+++ client/src/Component/GridPlayButton.tsx
@@ -4,7 +4,7 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 import type { GridRowId } from "@mui/x-data-grid";
 import { useAudioPlayerContext } from "../context/audio-player-context";
-import { OST_AUDIO_ENDPOINT } from "../config/api";
+import { OST_AUDIO_ENDPOINT, OST_IMAGE_ENDPOINT } from "../config/api";
 
 interface PlayButtonProps {
   rowId: GridRowId;
@@ -29,6 +29,15 @@ export const GridPlayButton = ({ rowId }: PlayButtonProps) => {
     }
   };
 
+  const fetchImage = async (rowId: GridRowId) => {
+    try {
+      const response = await fetch(`${OST_IMAGE_ENDPOINT}/${rowId}`);
+      return await response.text();
+    } catch (error) {
+      console.error("Failed to fetch image:", error);
+    }
+  };
+
   const onClick = async (rowId: GridRowId) => {
     audioRef.current?.pause();
     if (rowId === currentTrack.id && isPlaying) {
@@ -37,12 +46,14 @@ export const GridPlayButton = ({ rowId }: PlayButtonProps) => {
       const targetTrack = trackList.find((track) => track.id == rowId);
       setIsPlaying(true);
       const url = await fetchAudio(rowId);
+      const image = await fetchImage(rowId);
+
       setCurrentTrack({
         id: targetTrack?.id || 0,
         name: targetTrack?.name || "",
         src: url || "",
         author: targetTrack?.author || "",
-        thumbnail: "string",
+        thumbnail: image,
       });
     }
   };
--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL = "http://localhost:8080";
 export const OST_AUDIO_ENDPOINT = `${API_BASE_URL}/user/ost/audio`;
+export const OST_IMAGE_ENDPOINT = `${API_BASE_URL}/user/ost/image`;
