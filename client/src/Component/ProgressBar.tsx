e66d317ed4ccee8765f93c607d16fd8f8a65f86d
duck <simonchungcck42@gmail.com>
2025-10-07 15:01:30 +0800

    feat: basic styling for progress bar

--- client/src/Component/ProgressBar.tsx
+++ client/src/Component/ProgressBar.tsx
@@ -1,5 +1,22 @@
-const ProgressBar = () => {
-  return <div>ProgressBar</div>;
-};
+import { Box, Slider } from "@mui/material";
+
+const ProgressBar = () => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    width="100%"
+    gap={5} // gap-5 equivalent
+  >
+    <span>00:00</span>
+    <Slider
+      sx={{
+        maxWidth: "80%",
+        color: "primary",
+      }}
+    />
+    <span>03:34</span>
+  </Box>
+);
 
 export default ProgressBar;
