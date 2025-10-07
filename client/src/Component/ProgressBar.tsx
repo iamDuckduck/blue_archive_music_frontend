34b4dc224701a477605749867c22fe3ed8d69e0d
duck <simonchungcck42@gmail.com>
2025-10-07 15:53:38 +0800

    feat: Displaying time progress and duration for ProgressBar.tsx

--- client/src/Component/ProgressBar.tsx
+++ client/src/Component/ProgressBar.tsx
@@ -1,22 +1,50 @@
-import { Box, Slider } from "@mui/material";
+import { Box, Slider, styled, Typography } from "@mui/material";
+import { useState } from "react";
+import { useAudioPlayerContext } from "../context/audio-player-context";
 
-const ProgressBar = () => (
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    width="100%"
-    gap={5} // gap-5 equivalent
-  >
-    <span>00:00</span>
-    <Slider
-      sx={{
-        maxWidth: "80%",
-        color: "primary",
-      }}
-    />
-    <span>03:34</span>
-  </Box>
-);
+const ProgressBar = () => {
+  const TinyText = styled(Typography)({
+    fontSize: "0.75rem",
+    opacity: 0.38,
+    fontWeight: 500,
+    letterSpacing: 0.2,
+  });
+
+  const handleProgressChange = () => {
+    if (audioRef.current) setPosition(audioRef.current.currentTime);
+  };
+
+  function formatDuration(value: number) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
+  }
+
+  const { audioRef } = useAudioPlayerContext();
+  const duration = 200;
+
+  const [position, setPosition] = useState(0);
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      width="100%"
+      gap={5}
+    >
+      <TinyText>{formatDuration(position)}</TinyText>
+
+      <Slider
+        sx={{
+          maxWidth: "80%",
+        }}
+        defaultValue={position}
+        step={1} // For second-level precision
+        onChange={handleProgressChange}
+      />
+      <TinyText>-{formatDuration(duration - position)}</TinyText>
+    </Box>
+  );
+};
 
 export default ProgressBar;
