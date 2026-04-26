fd5d89422b21cb1401a1744100bf02ec5f25329f
duck <simonchungcck42@gmail.com>
2026-04-26 18:05:10 +0800

    feat(api): add getRandomSong client + Song entity

---
+++ client/src/entities/Song.ts
@@ -0,0 +1,12 @@
+export default interface Song {
+    title: string;
+    albumTitle: string;
+    artists: string[];
+    composers: string[];
+    audioPath: string;
+    imagePath: string;
+    description: string;
+    playCount: number;
+}
+
+
