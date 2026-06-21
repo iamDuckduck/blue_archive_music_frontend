8bc10cc0b716bfa45f5aac7e56b5a84ac4ed5061
duck <simonchungcck42@gmail.com>
2026-06-21 19:15:10 +0800

    chore: make frontend media URL configurable

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -8,4 +8,5 @@ export const OST_TYPE_BASE_ENDPOINT = "user/ostType";
 export const SONG_PLAY_COUNT_ENDPOINT = (songId: number) =>
   `/user/song/${songId}/play`;
 export const PUBLIC_URL_PREFIX =
+  import.meta.env.VITE_PUBLIC_MEDIA_BASE_URL ??
   "https://pub-185cd71ac9b44750afc23319e9e08130.r2.dev/";
